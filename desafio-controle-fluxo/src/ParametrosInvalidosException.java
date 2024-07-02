b08ef95b468eec25e801ea5b2616756de886fc84
Ana Peixoto <110309256+not-ana@users.noreply.github.com>
2024-07-01 22:40:38 -0300

    Add files via upload

---
+++ desafio-controle-fluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+public class ParametrosInvalidosException extends Exception {
+    
+    public ParametrosInvalidosException(String message) {
+        super("O segundo parâmetro deve ser maior que o primeiro");
+    }
+}
